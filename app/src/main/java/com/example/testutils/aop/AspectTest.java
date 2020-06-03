d670d54e863908324a2c98a32b4517cc1d8d223b
raotong <1037891202@qq.com>
2020-06-03 18:30:59 +0800

    aop拦截切点的方法修改

--- app/src/main/java/com/example/testutils/aop/AspectTest.java
+++ app/src/main/java/com/example/testutils/aop/AspectTest.java
@@ -4,11 +4,9 @@ import android.util.Log;
 
 import com.example.testutils.utils.NoDoubleClickUtils;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 
 /**
  * create at 2020/6/2
@@ -20,21 +18,24 @@ public class AspectTest {
     private String TAG = AspectTest.class.getSimpleName();
     private boolean isDoubleClick = false; // 用于判断是否重复点击
 
-    @Before("execution(@com.example.testutils.aop.DoubleClick  * *(..))")
-    public void beforeEnableDoubleClcik(JoinPoint joinPoint) throws Throwable {
+    @Around("execution(@com.example.testutils.aop.DoubleClick  * *(..))")
+    public void beforeEnableDoubleClcik(ProceedingJoinPoint joinPoint) throws Throwable {
         //用于处理切点所在的类
         Log.e(TAG, "beforeEnableDoubleClcik");
-        isDoubleClick = true;
+        if (!NoDoubleClickUtils.isDoubleClick()) {
+            joinPoint.proceed();
+            Log.e(TAG, "onClickLitener");
 
+        }
     }
 
     @Around("execution(* android.view.View.OnClickListener.onClick(..))")
     public void onClickLitener(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         // 用于处理切点标记的方法，和类
         Log.e(TAG, "onClickLitener");
-        if (isDoubleClick || !NoDoubleClickUtils.isDoubleClick()) {
+        if (!NoDoubleClickUtils.isDoubleClick()) {
             proceedingJoinPoint.proceed();
-            isDoubleClick = false;
+
         }
 
     }
